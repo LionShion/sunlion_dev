4bc9965a09637b77283f94e16c13b161504c1cff
SunLion <18625916235@163.com>
2024-04-10 10:01:38 +0800

    first commit

---
+++ src/main/java/com/example/sunlionlibrary/MyModuleTest.kt
@@ -0,0 +1,13 @@
+package com.example.sunlionlibrary
+
+/**
+ * Created by fengwj on 2024/4/9.
+ */
+class MyModuleTest {
+    /**
+     * 测试支持使用
+     */
+    fun getBoolean(): Boolean {
+        return true
+    }
+}
